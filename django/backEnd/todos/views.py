3f37bfe0b7d4d251cba47111d187cc3eefb33e74
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-05-21 05:21:20 +0100

    finish BE

--- django/backEnd/todos/views.py
+++ django/backEnd/todos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 
 # Create your views here.
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def list(self, request):
+        qq = self.get_queryset()
+        serializer = TodoSerializer(qq, many=True)
+        return Response(serializer.data)
+    
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
